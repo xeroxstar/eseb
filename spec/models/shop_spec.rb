1f68bc1f1023e840db0d7723fc90897a5ca9ab66
unknown <robdoan@.(none)>
2010-01-10 06:46:46 +0700

    define spec step for shop and shopowner

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -4,29 +4,39 @@ describe Shop do
   fixtures :shops
 
   before(:each) do
-    @user = stub_model(User,{
+    @shop_owner = stub_model(ShopOwner,{
         :login=>'quydoantran',
         :email=>'quydoantran@gmail.com'})
     @valid_attributes = {
       :name =>"Quynh Khanh",
       :shortname=>'quynhkhanh',
-      :owner=>@user
+      :owner=>@shop_owner
     }
   end
 
   it "should create a new instance given valid attributes" do
-    Shop.create!(@valid_attributes).should change(Shop,:count).by(1)
+    lambda{
+      Shop.create(@valid_attributes)
+    }.should change(Shop,:count).by(1)
   end
   describe "attributes" do
 
     it 'should have name' do
       create_shop({:name=>nil})
-      @shop.errors.on(:shortname).should_not be_nil
+      @shop.errors.on(:name).should_not be_nil
     end
 
     it 'should have shortname' do
       create_shop({:shortname=>nil})
       @shop.errors.on(:shortname).should_not be_nil
+    end
+
+    it 'should not duplicate shortname' do
+      lambda{
+        create_shop({:shortname=>'quynhkhanh'})
+        create_shop({:shortname=>'quynhkhanh'})
+        @shop.errors.on(:shortname).should_not be_nil
+      }.should_not change(Shop,:count).by(2)
     end
 
     describe 'disallow illegimate shortname:' do
@@ -59,6 +69,7 @@ describe Shop do
     end
 
     it 'should belong to user and user should have enough personal info' do
+      pending
       @shop.owner.should be_a_kind_of(User)
       @shop.owner.should be_enough_info
     end
@@ -74,7 +85,7 @@ describe Shop do
   end # End describe asscociation
 
   def create_shop(attrs={})
-    @valid_attributes.merge(attrs)
+    @valid_attributes = @valid_attributes.merge(attrs)
     @shop = Shop.create(@valid_attributes)
   end
 
