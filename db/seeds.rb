99fffa63454118cd9bc5865671703ebf6887bb19
rob doan <quydoantran@gmail.com>
2010-03-14 23:17:27 +0700

    Seed some subcategories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 require 'open-uri'
 require 'active_record/fixtures'
 
-#Country.delete_all
-#open("http://openconcept.ca/sites/openconcept.ca/files/country_code_drupal_0.txt") do |countries|
-#  countries.read.each_line do |country|
-#    code, name = country.chomp.split("|")
-#    Country.create!(:name => name, :code => code)
-#  end
-#end
+Country.delete_all
+open("http://openconcept.ca/sites/openconcept.ca/files/country_code_drupal_0.txt") do |countries|
+  countries.read.each_line do |country|
+    code, name = country.chomp.split("|")
+    Country.create!(:name => name, :code => code)
+  end
+end
 
 Category.delete_all
 Fixtures.create_fixtures("#{RAILS_ROOT}/spec/fixtures", "categories")
