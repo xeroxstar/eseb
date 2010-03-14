eb3b802a9ef5f0a11291df77ad10ba5a4679a6ea
rob doan <quydoantran@gmail.com>
2010-03-14 22:34:05 +0700

    Fixes migration file errors

--- db/migrate/20100314150331_rename_shortname_to_code_in_countries.rb
+++ db/migrate/20100314150331_rename_shortname_to_code_in_countries.rb
@@ -1,9 +1,9 @@
 class RenameShortnameToCodeInCountries < ActiveRecord::Migration
   def self.up
-    rename_column(:contries, :shortname, :code)
+    rename_column(:countries, :shortname, :code)
   end
 
   def self.down
-    rename_column(:contries, :code, :shortname)
+    rename_column(:countries, :code, :shortname)
   end
 end
